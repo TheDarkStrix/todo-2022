f05a1ebf15b1aba01e569125d9b11d4624ae3362
TheDarkStrix <thedarkstrix@gmail.com>
2020-11-16 14:40:23 +0530

    Dynamically read firebase db and display contents on screen realtime

--- components/notes/notes.js
+++ components/notes/notes.js
@@ -1,59 +1,58 @@
-import { Component } from "react";
+import { useState, useEffect } from "react";
 import ModalComponent from "../modal/model";
+import firebase from "../../utils/firebase";
 import style from "./notes.module.css";
 
-export class Notes extends Component {
-  render() {
-    var cardsData = [
-      {
-        title: "this is card1 title",
-        date: "May 20,2020",
-      },
-      {
-        title: "this is card2 title",
-        date: "May 22,2020",
-      },
-      {
-        title: "this is card3 title",
-        date: "May 28,2020",
-      },
-    ];
+export default function Notes() {
+  const [todoList, setTodoList] = useState([]);
 
-    return (
-      <>
-        <div className="row" style={{ maxWidth: "100%" }}>
-          <div className={["col-md-1", style.sidebar_parent].join(" ")}>
-            <div className={style.sidebar}>
-              <div className={style.heading}>TODO</div>
-              <div className={style.button_parent}>
-                <ModalComponent type="create" />
-              </div>
-            </div>
-          </div>
-          <div className="col">
-            <div className={style.button_parent_mobile}>
+  useEffect(() => {
+    const todoRef = firebase.database().ref("Todo");
+    todoRef.on("value", (snapshot) => {
+      const todos = snapshot.val();
+      const todoList = [];
+      for (let id in todos) {
+        todoList.push({ id, ...todos[id] });
+      }
+      console.log(todoList);
+      setTodoList(todoList);
+    });
+    console.log(todoList);
+  }, []);
+  return (
+    <>
+      <div className="row" style={{ maxWidth: "100%" }}>
+        <div className={["col-md-1", style.sidebar_parent].join(" ")}>
+          <div className={style.sidebar}>
+            <div className={style.heading}>TODO</div>
+            <div className={style.button_parent}>
               <ModalComponent type="create" />
-            </div>
-            <div className={style.cards}>
-              <div className="row" style={{ width: "100%" }}>
-                {" "}
-                {cardsData.map((data, i) => (
-                  <div className="col-md-4" key={i}>
-                    <div className={style.card}>
-                      <div className={style.title}>{data.title}</div>
-                      <div className={style.date}>{data.date}</div>
-                      <div className={style.edit}>
-                        <ModalComponent type="edit" />
-                      </div>
-                      <div className={style.star}></div>
-                    </div>
-                  </div>
-                ))}
-              </div>
             </div>
           </div>
         </div>
-      </>
-    );
-  }
+        <div className="col">
+          <div className={style.button_parent_mobile}>
+            <ModalComponent type="create" />
+          </div>
+          <div className={style.cards}>
+            <div className="row" style={{ width: "100%" }}>
+              {" "}
+              {todoList.map((data, i) => (
+                <div className="col-md-4" key={i}>
+                  <div className={style.card}>
+                    <div className={style.title}>{data.notes}</div>
+                    <div className={style.date}>{data.date}</div>
+                    <div className={style.edit}>
+                      <ModalComponent type="edit" />
+                    </div>
+                    <div className={style.star}></div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
