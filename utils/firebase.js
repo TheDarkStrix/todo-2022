45cce8e985e4f2ab799c12150c1aeb3d37b4454f
TheDarkStrix <thedarkstrix@gmail.com>
2022-05-26 22:21:01 +0530

    refactor: remove bootstrap , add tailwind/dayjs

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,14 +1,13 @@
 import firebase from "@firebase/app";
 import "@firebase/database";
 var firebaseConfig = {
-  apiKey: "AIzaSyAPs_Oo003_50F2Y71x7oMAGhGcVpbdGVE",
-  authDomain: "todo-geexec.firebaseapp.com",
-  databaseURL: "https://todo-geexec.firebaseio.com",
-  projectId: "todo-geexec",
-  storageBucket: "todo-geexec.appspot.com",
-  messagingSenderId: "31640005147",
-  appId: "1:31640005147:web:1ef5d41acc733e175f66be",
-  measurementId: "G-JPPFPRV549",
+  apiKey: "AIzaSyDaJwW8wKacabygXfhT9GzzTsT7WkPuCbg",
+  authDomain: "todoapp-7c108.firebaseapp.com",
+  databaseURL: "https://todoapp-7c108-default-rtdb.firebaseio.com",
+  projectId: "todoapp-7c108",
+  storageBucket: "todoapp-7c108.appspot.com",
+  messagingSenderId: "1051607102383",
+  appId: "1:1051607102383:web:b8fc0cd01738deee06355a",
 };
 // Initialize Firebase
 if (!firebase.apps.length) {
